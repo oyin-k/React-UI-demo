6e94b2f9726d7b78ed9c2dcc2ef5ad5d09c2c8dc
Oyinkan <oyink7@gmail.com>
2018-07-06 22:16:11 -0700

    removed the topicNumber file and added the component to the Topics component

--- react-ui-demo/src/Topics.js
+++ react-ui-demo/src/Topics.js
@@ -1,12 +1,24 @@
 import React, { Component }  from "react";
 import './App.css';
-import TopicNumber from './TopicNumber'
+
 
 class Topics extends Component {
+    state = {numberActive: null}
+    showTopicNumberActive = () => {
+        this.setState({
+            numberActive: 1
+        })
+    } 
+
+    componentDidMount() {
+        this.showTopicNumberActive();
+    }
+
     render() {
+        const {numberActive} = this.state;
         return (
             <div className="topics">
-                <TopicNumber />
+                <TopicNumber numberActive={numberActive}/>
                 <ul className="nav flex-column">
                     <li className="nav-item">
                         <a className="nav-link active btn btn-success" href="#">Guidance</a>
@@ -21,6 +33,14 @@ class Topics extends Component {
             </div>
         );
     }
-} 
+}
+
+function TopicNumber({numberActive}) {
+    return(
+        <div className="topic-number">
+            <span>{numberActive}</span>
+        </div>
+    )
+}
 
 export default Topics;
